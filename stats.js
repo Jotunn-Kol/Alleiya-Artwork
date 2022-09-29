eb65dbff86e09960a2b31ccd2e93d6f9b8325aa1
Patrick Burdette <patrick.a.burdette@gmail.com>
2022-09-29 13:12:11 -0500

    unable to finish javascript for stat rolling before code freeze.

---
+++ stats.js
@@ -0,0 +1,10 @@
+const fifth = document.getElementById('fifth-stats')
+const three = document.getElementById('three-stats')
+const path = document.getElementById('path-stats')
+const rifts = document.getElementById('rifts-stats')
+
+fifth.addEventListener('click', () => {
+    document.getElementsByName('Email').placeholder={
+        'new text for email';
+
+})
